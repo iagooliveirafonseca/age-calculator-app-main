3cc2ffe6c9684376460a45d27d12aa9c9b9bb283
Iago Oliveira <Iagogoddigital@hotmail.com>
2024-02-17 14:34:05 -0300

    Começando a lógica da idade

--- JS/script.js
+++ JS/script.js
@@ -1,15 +1,41 @@
 const allInputs = document.querySelectorAll("input");
 const allSpans = document.querySelectorAll("span");
 const allDates = document.querySelectorAll(".time");
-const paragraphTime = document.querySelectorAll(".takeParagrafhTime")
-const getDate = new Date()
-const dayNow = getDate.getDate()
-const monthNow = getDate.getMonth()
-const yearNow = getDate.getFullYear()
+const paragraphTime = document.querySelectorAll(".takeParagrafhTime");
+const getInformationOfDate = new Date();
+const dayNow = getInformationOfDate.getDate();
+const monthNow = getInformationOfDate.getMonth();
+const yearNow = getInformationOfDate.getFullYear();
 
-console.log(dayNow)
-console.log(monthNow)
-console.log(yearNow)
+function calcularIdade() {
+  var idade = yearNow - allInputs[2].value;
+  var mes = (monthNow + 1) - allInputs[1].value ;
+  var dia = allInputs[0].value - dayNow ;
+
+  if (dia <= 0 && mes <= 0) {
+    idade = idade;
+    console.log(idade)
+    console.log(mes)
+    console.log(dia)
+   
+  } else {
+    idade = idade - 1;
+    mes += 12
+    console.log(idade);
+    console.log(mes)
+    console.log(dia)
+  
+  }
+
+  if(dia <=0){
+    idade = idade
+  } else{
+    idade ++
+    new Date(yearNow, monthNow, 0).getDate() + dia 
+  
+  }
+
+}
 
 function empty(index) {
   allSpans[index].style.display = "block";
@@ -19,15 +45,19 @@ function notEmpty(index) {
   allSpans[index].style.display = "";
 }
 
-function ageValid(index) {
-  if (allInputs[index].value > 31 || allInputs[index].value <= 0) {
+function validDayAndMonth(index) {
+  if (
+    allInputs[index].value > 31 ||
+    allInputs[index].value <= 0 ||
+    allInputs[1].value > 12
+  ) {
     allSpans[index].style.display = "block";
-    allSpans[index].textContent = `Must be a valid ${paragraphTime[index].textContent}`;
+    allSpans[
+      index
+    ].textContent = `Must be a valid ${paragraphTime[index].textContent}`;
   } else {
     allSpans[index].style.display = "";
-    allSpans[index].textContent = "This Field is required" ;
-
-
+    allSpans[index].textContent = "This Field is required";
   }
 }
 
@@ -36,8 +66,8 @@ function validDate() {
     empty(0);
   } else {
     notEmpty(0);
-    allDates[2].textContent = `${allInputs[0].value - dayNow}`;
-    ageValid(0);
+    allDates[2].textContent = `${allInputs[0].value}`;
+    validDayAndMonth(0);
   }
 }
 
@@ -46,9 +76,18 @@ function validMonth() {
     empty(1);
   } else {
     notEmpty(1);
-    allDates[1].textContent = `${12 - allInputs[1].value}`;
-        ageValid(1);
+    allDates[1].textContent = `${allInputs[1].value}`;
+    validDayAndMonth(1);
   }
+}
+
+function checkBirthday() {
+  if (allInputs[1] > monthNow) {
+    console.log(yearNow);
+  } else {
+    return (yearNow = yearNow - 1);
+  }
+  return;
 }
 
 function validYear() {
@@ -56,7 +95,17 @@ function validYear() {
     empty(2);
   } else {
     notEmpty(2);
-    allDates[0].textContent = `${yearNow - allInputs[2].value }`;
-        ageValid(2);
+
+    function validYear() {
+      if (allInputs[2].value > yearNow || allInputs[2].value < 1875) {
+        allSpans[2].style.display = "block";
+        allSpans[2].textContent = `Must be a valid ${paragraphTime[2].textContent}`;
+      } else {
+        allSpans[2].style.display = "";
+        allSpans[2].textContent = "This Field is required";
+      }
+    }
+    validYear();
+    allDates[0].textContent = `${yearNow - allInputs[2].value}`;
   }
 }
